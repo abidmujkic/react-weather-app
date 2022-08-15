2d2d5b58cbb449d275093f0f88358cdbeea041bf
abidmujkic <abidmujkic1@gail.com>
2022-08-15 16:28:44 +0200

    commit

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '35b9461efamsh13e369b094e8268p13f968jsn8b276a299e6e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "5e77ed1e56a1ce96d824813f8c5a56c5";
